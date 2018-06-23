33593c2392c3e45c8b52500cdb0bc46d1e9fdfd9
ektravel <38017980+ektravel@users.noreply.github.com>
2018-06-22 19:24:07 -0500

    added functions to shuffle and update topscore

--- src/App.js
+++ src/App.js
@@ -12,23 +12,62 @@ class App extends Component {
     message: "Click any image to begin.",
     ducktales,
     score: 0,
-    topscore: 0
+    topscore: 0,
+    clickedID: []
   };
 
-//clickDuck function increases this.state.score by 1
+//function to shuffle the array everytime an item is clicked
+shuffleData = data => {
+  let i = data.length - 1;
+  while (i > 0) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
+    i--;
+  }
+  return data;
+};
+  
+//function to update the topscore
+getTopscore = currentScore =>{
+  if (currentScore > this.state.topscore)
+    return currentScore;
+  else
+    return this.state.topscore;
+};
+
 clickDuck = id => {
-  //setState method is used to update a component's state
-  this.state.score = this.state.score + 1;
+//check if it's been clicked on before
+  //if it has NOT been clicked before => check to see if all other cards have been clicked =>
+      // if YES => they win the game
+      //if NO => update the score +1 and re-shuffle the array
+  if (this.state.clickedID.indexOf(id) === -1 ){
+    let newArray = [...this.state.clickedID];
+    let newDucktales = [...this.state.ducktales];
+    newArray.push(id);
+    //setState method is used to update a component's state
+    this.setState({
+      score: this.state.score + 1,
+      clickedID: newArray,
+      ducktales: this.shuffleData(newDucktales), 
+      message: "Well done! That was correct!",
+      topscore: this.getTopscore(this.state.score)
+    })
+    
+  } else {
+    //if it has been clicked before => reset the score and restart the game
+    let newDucktales = [...this.state.ducktales];
 
-  // const updatedState = this.state;
-
-
-  // this.setState({ updatedState });
-  // this.setState({score: this.state.score + 1});
-
-  //if the same card is clicked twice, set the score back to 0
-
-  //after each reset, update the topscore if the number is higher than before
+    //setState method is used to update a component's state
+    this.setState({
+      score: 0,
+      message: "That one has been clicked before! Game over.",
+      ducktales: this.shuffleData(newDucktales),
+      clickedID: [],
+      topscore: this.getTopscore(this.state.score)
+    })
+  };
 };
 
 //Map over this.state.ducktales and render a DucktalesCard component for each duck object
